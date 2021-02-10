6ebcc0e9f5c63d9de225e78bf699b066db4105af
Brian-Demon <BrianWMatejka@gmail.com>
2021-02-10 12:02:01 -0600

    initial

---
+++ lib/get_weather.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "get_weather/version"
+
+module GetWeather
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/get_weather/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GetWeather
+  VERSION = "0.1.0"
+end
