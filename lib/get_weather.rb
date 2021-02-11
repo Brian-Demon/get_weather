60376d5aaa87a9f950a665c93e55a8aaa8da91a6
Brian-Demon <BrianWMatejka@gmail.com>
2021-02-11 15:19:28 -0600

    WIP

--- lib/get_weather.rb
+++ lib/get_weather.rb
@@ -1,8 +1,62 @@
 # frozen_string_literal: true
 
 require_relative "get_weather/version"
+require "faraday"
+require "json"
 
 module GetWeather
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError
+  end
+  
+  class Weather
+    attr_reader :temp, :description
+
+    $LAT = 41.936748
+    $LONG = -88.069309
+    $APPID = "a69d47752b3fca28f70d731e9447c84a"
+
+    def initialize()
+      @temp = "No Temp Yet!"
+      @description = "No Description Yet!"
+    end
+
+    def get_daily(weather_hash)
+      @temp = weather_hash["daily"][0]["temp"]["day"]
+      @description = weather_hash["daily"][0]["weather"][0]["description"]
+      to_s
+    end
+
+    def get_current(weather_hash)
+      @temp = weather_hash["current"]["temp"]
+      @description = weather_hash["current"]["weather"][0]["description"]
+      to_s
+    end
+
+    def to_s
+      <<-EOF
+Weather for Lat: #{$LAT}, Long: #{$LONG}: #{@temp}ºF, #{@description}
+      EOF
+    end
+  end
+
+  def self.get_weather(output: $stdout, client: faraday_client, forecast: forecast_passed)
+    response = client.get("/data/2.5/onecall?lat=#{$LAT}&lon=#{$LONG}&appid=#{$APPID}")
+    if response.success?
+      weather_hash = JSON.parse(response.body)
+      weather = Weather.new()
+      output.puts weather.get_daily(weather_hash) if forecast.downcase == "daily"
+      output.puts weather.get_current(weather_hash) if forecast.downcase == "current"
+    else
+      output.puts "Not successful"
+    end
+
+    def self.faraday_client
+      Faraday.new("https://api.openweathermap.org")
+    end
+  end
+
+  if $0 == __FILE__
+    Weather.get_weather
+  end
+
 end
--- test/get_weather_test.rb
+++ test/get_weather_test.rb
@@ -1,13 +1,62 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "stringio"
 
 class GetWeatherTest < Minitest::Test
+  def canned_weather_hash
+    {
+      "daily" => [
+        {
+          "temp" => {
+            "day" => "2"
+            },
+          "weather" => [
+            {
+              "description" => "Snow"
+            }
+          ]
+        }
+      ]
+    }
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::GetWeather::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def get_output_client_hash
+    output = StringIO.new
+    client = Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.get("/data/2.5/onecall?lat=#{41.936748}&lon=#{-88.069309}&appid=#{"a69d47752b3fca28f70d731e9447c84a"}") { |env| [200, {}, JSON.generate(canned_weather_hash)] }
+      end
+    end
+    { "output" => output, "client" => client }
   end
+
+  def test_gets_successful_response_from_API
+    #skip
+    output_client_hash = get_output_client_hash
+    output = output_client_hash["output"]
+    client = output_client_hash["client"]
+    forecast = "current"
+    GetWeather.get_weather(output: output, client: client, forecast: forecast)
+    output.rewind
+    should_not_equal = "Not successful"
+    refute should_not_equal, output.read.chomp
+  end
+
+  def test_that_get_daily_prints_temp_and_weather
+    #skip
+    output_client_hash = get_output_client_hash
+    output = output_client_hash["output"]
+    client = output_client_hash["client"]
+    forecast = "daily"
+    GetWeather.get_weather(output: output, client: client, forecast: forecast)   
+    output.rewind
+    expected = "Weather for Lat: 41.936748, Long: -88.069309: 2ºF, Snow"
+    assert_equal expected, output.read.chomp
+  end
+
 end
